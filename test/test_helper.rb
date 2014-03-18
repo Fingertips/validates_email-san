49e033495815ded9e73324baacb5fdd67d34dbe9
Manfred Stienstra <manfred@fngtps.com>
2014-03-18 11:28:58 +0100

    Don't try to load Rails from vendor in tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,35 +1,18 @@
 module ValidatesEmailSanTest
   module Initializer
-    VENDOR_RAILS = File.expand_path('../../../../../vendor/rails', __FILE__)
-    OTHER_RAILS = File.expand_path('../../../rails', __FILE__)
-    PLUGIN_ROOT = File.expand_path('../../', __FILE__)
-    
-    def self.rails_directory
-      if File.exist?(VENDOR_RAILS)
-        VENDOR_RAILS
-      elsif File.exist?(OTHER_RAILS)
-        OTHER_RAILS
-      end
-    end
-    
     def self.load_dependencies
-      if rails_directory
-        $:.unshift(File.join(rails_directory, 'activesupport', 'lib'))
-        $:.unshift(File.join(rails_directory, 'activerecord', 'lib'))
-      else
-        require 'rubygems' rescue LoadError
-      end
-      
       require 'test/unit'
       
       require 'active_support'
       require 'active_support/test_case'
       require 'active_record'
-      require 'active_record/test_case'
-      require 'active_record/base' # this is needed because of dependency hell
+      
+      if const_defined?(:I18n) && I18n.respond_to?(:enforce_available_locales)
+        I18n.enforce_available_locales = true
+      end
       
       $:.unshift File.expand_path('../../lib', __FILE__)
-      require File.join(PLUGIN_ROOT, 'rails', 'init')
+      require File.expand_path('../../rails/init', __FILE__)
     end
     
     def self.configure_database
